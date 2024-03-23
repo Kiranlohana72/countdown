89a2f0f569543f4c9d45bc6cd8783306f133bc95
its_YA_giRl_kiRan(Psychologist) <160026867+Kiranlohana72@users.noreply.github.com>
2024-03-24 04:01:35 +0500

    Update index.js some changes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //Set the date we're counting down to 
-var countDownDate = new Date("march 25, 2024 00:00:00").getTime();
+let countDownDate = new Date("march 25, 2024 00:00:00").getTime();
 
 //Update the count down every 1 sec
-var x = setInterval(function(){
+let x = setInterval(function(){
 
     //Get today's date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     //Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    let distance = countDownDate - now;
 
     //Time calculations for days, hours, minutes and seconds
-    var days = Math.floor (distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor ((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor ((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor ((distance % (1000 * 60)) / 1000);
+    let days = Math.floor (distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor ((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor ((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor ((distance % (1000 * 60)) / 1000);
 
     //Output the result to an element
     document.getElementById("days").innerHTML = days + "<br><span>days</span>";
@@ -26,7 +26,7 @@ var x = setInterval(function(){
     if(distance < 0){
         clearInterval(x);
         document.getElementById("time").style.display = "none";
-        document.querySelector("h1").className = "new-year";
+        document.querySelector("h1").className = "holi";
 
         //firework animation
         const fireworkCount = 350;
